aa22ce81b8ca4fde45b39564abf788f2696c240f
Dima Roschin <ardi.2005myros@gmail.com>
2024-05-31 21:26:08 +0700

    start

---
+++ pages/index.vue
@@ -0,0 +1,21 @@
+<script setup lang="ts">
+
+</script>
+
+<template>
+  <div class="body">
+    <h2>Комплекты стеллажных систем</h2>
+
+  </div>
+</template>
+
+<style scoped>
+.body {
+  height: 70vh;
+  margin: 4% 10%;
+}
+h1 {
+  margin: 0;
+  padding: 0;
+}
+</style>
